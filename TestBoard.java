539be86d22f0cca77a40f16e7d6b5c4dc8caa562
zhangkai <zhangkai1211@qq.com>
2020-02-10 21:35:42 +0100

    Exercice 4

--- TestBoard.java
+++ TestBoard.java
@@ -4,7 +4,9 @@ public class TestBoard {
     public static void main(String [] args){
         Board board=new Board("A");
         Submarine submarine=new Submarine('S');
-        board.putShip(submarine,2,3);
+        Destroyer destroyer=new Destroyer('D');
+        board.putShip(submarine,8,3);
+        board.putShip(destroyer,1,3);
         board.setHit(true,4,4);
         board.print();
         System.out.println(board.hasShip(7,3));
